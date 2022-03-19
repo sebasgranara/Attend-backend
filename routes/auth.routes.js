8d9ec2026cf9781be740422eade3ba13e13bef14
Sebas Granara <sebastian.granara@gmail.com>
2022-03-19 14:24:25 +0100

    udpate

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User.model');
+const User = require('../models/user.model');
 const { isAuthenticated } = require('../middleware/jwt.middleware');
 
 const router = express.Router();
